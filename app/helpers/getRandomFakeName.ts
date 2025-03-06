47d615c941856e675759e0f4f68d92941734adaf
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2025-03-05 21:07:25 -0300

    atualiza senhas

--- app/helpers/getRandomFakeName.ts
+++ app/helpers/getRandomFakeName.ts
@@ -1,11 +1,15 @@
-export function getRandomFakeName(name: string) {
-  const nameParts = name.split(" ")
+function removeAccents(str:string) {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Remove diacríticos manualmente
+}
+
+export function getRandomFakeName(name:string) {
+  const normalizedName = removeAccents(name);
+  const nameParts = normalizedName.split(" ");
   const parte1 = nameParts[0];
-  const parte2 = nameParts.length > 1 ? nameParts[1] : ""
+  const parte2 = nameParts.length > 1 ? nameParts[1] : "";
 
-  // gerar dois números aleatórios entre 10 e 99
-  var num1 = Math.floor(Math.random() * 90) + 10;
-
+  // Gerar dois números aleatórios entre 10 e 99
+  const num1 = Math.floor(Math.random() * 90) + 10;
 
   const nomeDeUsuario = `${parte1.substring(0, 3)}${parte2.substring(0, 5)}${num1}`;
   return nomeDeUsuario.toLowerCase();
